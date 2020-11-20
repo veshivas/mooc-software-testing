b5030893f707d4c1a515487097abeede18ba5495
Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>
2020-11-20 23:58:28 +0100

    Topic 4: Excercise #1

--- src/main/java/tudelft/sum/TwoNumbersSum.java
+++ src/main/java/tudelft/sum/TwoNumbersSum.java
@@ -1,12 +1,25 @@
 package tudelft.sum;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 // Source: https://leetcode.com/problems/add-two-numbers/description/
 class TwoNumbersSum {
+    private ArrayList<Integer> list_one;
+    private ArrayList<Integer> list_two;
 
-    public ArrayList<Integer> addTwoNumbers(ArrayList<Integer> first, ArrayList<Integer> second) {
+    public TwoNumbersSum(Integer[] first, Integer[] second) {
+        assert (first[0] != 0 || second[0] != 0);
+        this.list_one = new ArrayList<Integer>();
+        this.list_one.addAll(Arrays.asList(first));
+        this.list_two = new ArrayList<Integer>();
+        this.list_two.addAll(Arrays.asList(second));
+    }
+
+    public ArrayList<Integer> addTwoNumbers() {
+        ArrayList<Integer> first = this.list_one;
+        ArrayList<Integer> second = this.list_two;
         Collections.reverse(first);
         Collections.reverse(second);
 
--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -1,5 +1,36 @@
 package tudelft.sum;
 
-public class TwoNumbersSumTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+public class TwoNumbersSumTest {
+    @Test
+    public void numbers_without_zero() {
+        Integer[] one = {1, 2};
+        Integer[] two = {3, 4};
+        TwoNumbersSum sum_num = new TwoNumbersSum(one,two);
+        ArrayList<Integer> result = sum_num.addTwoNumbers();
+        Integer[] expected = {4, 6};
+        Assertions.assertArrayEquals(expected,result.toArray());
+    }
+    @Test
+    public void numbers_with_zero() {
+        Integer[] one = {1, 0, 3};
+        Integer[] two = {2, 2, 8};
+        TwoNumbersSum sum_num = new TwoNumbersSum(one, two);
+        ArrayList<Integer> result = sum_num.addTwoNumbers();
+        Integer[] expected = {3, 3, 1};
+        Assertions.assertArrayEquals(expected,result.toArray());
+    }
+    @Test
+    public void numbers_non_matchng_digits() {
+        Integer[] one = {1, 2, 3};
+        Integer[] two = {9, 8};
+        TwoNumbersSum sum_num = new TwoNumbersSum(one, two);
+        ArrayList<Integer> result = sum_num.addTwoNumbers();
+        Integer[] expected = {2, 2, 1};
+        Assertions.assertArrayEquals(expected,result.toArray());
+    }
 }
