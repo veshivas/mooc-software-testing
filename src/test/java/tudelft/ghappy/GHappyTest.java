171ea618531b5087453abdf1e7254d93e72d11f8
Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>
2020-11-20 23:21:44 +0100

    Excercise #1

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,25 @@
 package tudelft.ghappy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tudelft.mirror.Mirror;
 
 public class GHappyTest {
+    @Test
+    public void g_is_happy() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("buggy");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void g_is_not_happy() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("xxggyygxx");
+        Assertions.assertFalse(result);
+    }
+    @Test
+    public void g_is_not_found() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("be");
+        Assertions.assertFalse(result);
+    }
 }
