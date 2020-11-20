39c7e55e713552344d889fae1bb974f3018579f6
Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>
2020-11-21 00:02:56 +0100

    RomanNumeralTest fix

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,35 +1,39 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+    private RomanNumeral roman;
+
+    @BeforeEach
+    public void initialize() {
+        this.roman = new RomanNumeral();
+        System.out.print("This method is called before each test!\n");
+    }
 
     @Test
     public void singleNumber() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("I");
-        Assertions.assertEquals(1, result);
-    }
-
-    @Test
-    public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("VIII");
-        Assertions.assertEquals(8, result);
-    }
-
-    @Test
-    public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
+    public void numberWithManyDigits() {
+        int result = roman.convert("VII");
+        Assertions.assertEquals(7, result);
+    }
+
+    @Test
+    public void numberWithSubtractiveNotation() {
+        int result = roman.convert("IV");
+        Assertions.assertEquals(6, result);
+    }
+
+    @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
